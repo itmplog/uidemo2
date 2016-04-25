eeecf6568057b2394b14e7ad6290fd6ad17b2f19
huaixiaoz <hello@itmp.top>
2016-04-25 22:54:33 +0800

    dummy support

--- app/src/main/java/top/itmp/uidemo2/ui/MainActivity.java
+++ app/src/main/java/top/itmp/uidemo2/ui/MainActivity.java
@@ -2,20 +2,26 @@ package top.itmp.uidemo2.ui;
 
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerTitleStrip;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SlidingPaneLayout;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 import top.itmp.uidemo2.base.BaseActivity;
 
 import top.itmp.uidemo2.R;
+import top.itmp.uidemo2.utils.Dummy;
 
 public class MainActivity extends BaseActivity {
 
     private SlidingPaneLayout mSlidingPaneLayout;
     private ListView mListView;
     private FrameLayout mFrameLayout;
+    private ViewPager mViewPager;
+    private PagerTitleStrip mPagerTitleStrip;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,7 +29,9 @@ public class MainActivity extends BaseActivity {
 
         mSlidingPaneLayout = (SlidingPaneLayout)findViewById(R.id.sliding_pane_layout);
         mListView = (ListView)findViewById(R.id.left_pane);
-        mFrameLayout = (FrameLayout)findViewById(R.id.context);
+        //mFrameLayout = (FrameLayout)findViewById(R.id.context);
+        mViewPager = (ViewPager)findViewById(R.id.pager);
+        mPagerTitleStrip = (PagerTitleStrip)findViewById(R.id.titles);
 
         mSlidingPaneLayout.setSliderFadeColor(ContextCompat.getColor(this, R.color.translucent));
         mSlidingPaneLayout.setPanelSlideListener(new SlidingPaneLayout.PanelSlideListener() {
@@ -42,5 +50,8 @@ public class MainActivity extends BaseActivity {
 
             }
         });
+
+        mListView.setAdapter(new SimpleAdapter(this, Dummy.dummyListMap("dummy"), android.R.layout.simple_list_item_1,
+                new String[]{"dummy"}, new int[]{android.R.id.text1}));
     }
 }
